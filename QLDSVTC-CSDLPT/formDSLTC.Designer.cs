19c76e8c84d8dcaa7b8ab8efbce20bf9e2ac6d43
vanchau <vovanchau2000@gmail.com>
2022-05-29 01:04:42 +0700

    tao xong dsltc

---
+++ QLDSVTC-CSDLPT/formDSLTC.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace QLDSVTC_CSDLPT
+{
+    partial class formDSLTC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.lbkhoa = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbkhoa = new System.Windows.Forms.ComboBox();
+            this.cbnienkhoa = new System.Windows.Forms.ComboBox();
+            this.cbhocky = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Appearance.BackColor = System.Drawing.Color.Silver;
+            this.panelControl1.Appearance.Options.UseBackColor = true;
+            this.panelControl1.Controls.Add(this.button2);
+            this.panelControl1.Controls.Add(this.button1);
+            this.panelControl1.Controls.Add(this.cbhocky);
+            this.panelControl1.Controls.Add(this.cbnienkhoa);
+            this.panelControl1.Controls.Add(this.cbkhoa);
+            this.panelControl1.Controls.Add(this.label3);
+            this.panelControl1.Controls.Add(this.label2);
+            this.panelControl1.Controls.Add(this.lbkhoa);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(1206, 272);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // lbkhoa
+            // 
+            this.lbkhoa.AutoSize = true;
+            this.lbkhoa.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbkhoa.Location = new System.Drawing.Point(343, 76);
+            this.lbkhoa.Name = "lbkhoa";
+            this.lbkhoa.Size = new System.Drawing.Size(58, 23);
+            this.lbkhoa.TabIndex = 0;
+            this.lbkhoa.Text = "KHOA";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Niên Khóa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(335, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Học kỳ";
+            // 
+            // cbkhoa
+            // 
+            this.cbkhoa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbkhoa.FormattingEnabled = true;
+            this.cbkhoa.Location = new System.Drawing.Point(414, 76);
+            this.cbkhoa.Name = "cbkhoa";
+            this.cbkhoa.Size = new System.Drawing.Size(217, 24);
+            this.cbkhoa.TabIndex = 3;
+            // 
+            // cbnienkhoa
+            // 
+            this.cbnienkhoa.FormattingEnabled = true;
+            this.cbnienkhoa.Items.AddRange(new object[] {
+            "2017-2018",
+            "2018-2019",
+            "2019-2020",
+            "2020-2021",
+            "2021-2022",
+            "2022-2023",
+            "2023-2024",
+            "2024-2025",
+            "2025-2026"});
+            this.cbnienkhoa.Location = new System.Drawing.Point(414, 112);
+            this.cbnienkhoa.Name = "cbnienkhoa";
+            this.cbnienkhoa.Size = new System.Drawing.Size(217, 24);
+            this.cbnienkhoa.TabIndex = 4;
+            // 
+            // cbhocky
+            // 
+            this.cbhocky.FormattingEnabled = true;
+            this.cbhocky.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cbhocky.Location = new System.Drawing.Point(414, 147);
+            this.cbhocky.Name = "cbhocky";
+            this.cbhocky.Size = new System.Drawing.Size(217, 24);
+            this.cbhocky.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(414, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Xem trước";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(556, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // formDSLTC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 565);
+            this.Controls.Add(this.panelControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "formDSLTC";
+            this.Text = "Danh sách lớp tín chỉ";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbhocky;
+        private System.Windows.Forms.ComboBox cbnienkhoa;
+        private System.Windows.Forms.ComboBox cbkhoa;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbkhoa;
+    }
+}
